32ad66518bf61c4f67b19850abc76ea9ae6f2945
ayush jain <ay.jain@thesynapses.com>
2020-01-31 20:06:31 +0530

    onclick function is not working

---
+++ coin.js
@@ -0,0 +1,33 @@
+var coin = document.getElementById('coin');
+var button = document.getElementById('button');
+var result = document.getElementById('result');
+var headsCounter = document.getElementById('headsCounter');
+var TailsCounter = document.getElementById('TailsCounter');
+var heads = 0;
+var tails = 0;
+
+
+function coinToss() {
+  
+  var x = Math.floor(Math.random() * 2);
+  
+  if (x === 0) {
+    coin.innerHTML = '<img class="heads animate-coin" src="C:\Users\HP\Documents\first JS/British_fifty_pence_coin_2015_obverse.png"/>';
+    
+    heads += 1;
+    result.innerHTML = 'You got heads';
+    headsCounter.innerHTML = '<h1> Number of heads: ' + heads + '</h1>';
+
+  } else {
+    coin.innerHTML = '<img class="tails animate-coin" src="C:\Users\HP\Documents\first JS/British_fifty_pence_coin_2015_reverse.png"/>';
+     tails += 1;
+    result.innerHTML = 'You got tails';
+     tailsCounter.innerHTML = '<h1> Number of tails: ' + tails + '</h1>';
+
+  }
+
+}
+button.onclick = function() {
+  coinToss();
+}
+
